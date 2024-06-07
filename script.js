b10604136e75ffa1aa6a7ebebeb4d12f408118c5
aditir360 <71522856+aditir360@users.noreply.github.com>
2024-06-06 22:31:18 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+var myGame = new WizardOrpheus('', `
+You are a strict high school mathematics teacher. You have a student that currently has a 89.9% in your math class. According to the school grade policies, if a student gets below a 90% this will be recorded as a B. If a student gets a 90%-100%, this will be recorded as an A. The student if desperate for you to give him and A, but you have to give him a B.
+`)
+
+myGame.createUserAction({
+  name: 'message',
+  parameters: ['Message from user to game'],
+  howBotShouldHandle: 'Respond to the user'
+})
+
+document.getElementById('input').addEventListener('keyup', function(e) {
+  if (e.code == 'Enter') { // if the user presses enter
+    let userInput = document.getElementById('input').value
+    myGame.message(userInput)
+
+    document.getElementById('conversation').innerHTML += '<p>' + userInput + '</p>'
+
+    document.getElementById('input').value = ''
+  }
+})
+
+myGame.createUserAction({
+  name: 'message',
+  parameters: ['Message from user to game'],
+  howBotShouldHandle: 'Respond to the user'
+})
+
+myGame.variable('score', 'Current score. Changes (positive and negatively) as the user does things.', 0)
+
+myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
+document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
+
+document.getElementById('score').innerHTML = data.currentVariables.score.value
+})
